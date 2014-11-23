3248877e4e5a40c38721c2271c14659a1117ef5c
Vitaly Egorov <egorov@samsonos.com>
2014-11-23 16:58:29 +0200

    Added phpunit bootstrap and config

---
+++ phpunit.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>
+ * on 04.08.14 at 17:43
+ */
+
+/** Set composer autoloader */
+require __DIR__.'/vendor/autoload.php';
+
+/** Set The Default Timezone */
+date_default_timezone_set('UTC');
